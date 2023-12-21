dd23943257b428036567c3427565a3d4bf3db289
Rohit Bajiya <okbajiyaji@gmail.com>
2023-12-21 09:36:51 +0530

    Employee

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\Team;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Employee extends Model
 {
